b8a4821be422d57a17557f01a93bea3c6ffc3409
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-02-28 07:26:30 -0500

    sort number with function

--- 6_transform.js
+++ 6_transform.js
@@ -59,3 +59,15 @@
 // console.log( arr );  // 1, 15, 2
 
 // To use our own sorting order, we need to supply a function as the argument of arr.sort().
+
+function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+}
+
+let arr = [1, 2, 15];
+
+arr.sort(compareNumeric);
+
+console.log(arr);  // 1, 2, 15
