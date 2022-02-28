6834b813127bbac148454f051aa62cdbac08c25f
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-02-28 06:34:04 -0500

    example 1

--- 5_searching.js
+++ 5_searching.js
@@ -10,3 +10,11 @@
 // found, otherwise -1.
 // arr.lastIndexOf(item, from) – same, but looks for from right to left.
 // arr.includes(item, from) – looks for item starting from index from, returns true if found.
+
+let arr = [1, 0, false];
+
+console.log( arr.indexOf(0) ); // 1
+console.log( arr.indexOf(false) ); // 2
+console.log( arr.indexOf(null) ); // -1
+
+console.log( arr.includes(1) ); // true
