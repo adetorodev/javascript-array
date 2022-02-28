51c40cfbd5e20639ab6c5040281b9a30b067a03d
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-02-28 07:07:01 -0500

    map more

--- 6_transform.js
+++ 6_transform.js
@@ -35,3 +35,17 @@
 // })
 
 // console.log(userFullnames);
+
+let arr = [2, 3, 5, 7]
+
+// arr.map(function (element, index, array) {
+//     console.log(this) // 80
+// }, 80);
+
+arr.map(function (element, index, array) {
+    console.log(element);
+    console.log(index);
+    console.log(array);
+    return element;
+}, 80);
+
