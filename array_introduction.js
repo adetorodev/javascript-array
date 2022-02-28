82c080604b33078d583cf732a2c8f1797bfadcb7
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-02-28 03:53:35 -0500

    first commit

---
+++ array_introduction.js
@@ -0,0 +1,17 @@
+// Let us create an array of numbers
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numbers);
+
+// To run the code use node array_introduction.js in the terminal
+
+// let get items from the array. We simply need to use the index of the array
+/* How to get the array index 
+    1  2  3  4  5  6  7  8  9  10
+    0  1  2  3  4  5  6  7  8  9
+*/
+// We can get the first item in the array by using the index 0
+console.log(numbers[0]);
+// We can get the last item in the array by using the index 9
+console.log(numbers[9]);
+// We can get the middle item in the array by using the index 4
+console.log(numbers[4]);
