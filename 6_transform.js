3673e9903d3b67d445dc646ad7cbff2259c563b5
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-02-28 06:56:54 -0500

    transform intro

---
+++ 6_transform.js
@@ -0,0 +1,17 @@
+/* *************************************
+        Transform an array
+*************************************** */
+
+// Let’s move on to methods that transform and reorder an array.
+
+// map
+// The arr.map method is one of the most useful and often used.
+
+// It calls the function for each element of the array and returns the array of results.
+
+// The syntax is:
+
+// let result = arr.map(function(item, index, array) {
+//   // returns the new value instead of item
+// });
+
