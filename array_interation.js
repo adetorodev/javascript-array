b826ed7c5c15c79e848f6bdb06a4300053a38372
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-02-28 06:25:32 -0500

    interation update

--- array_interation.js
+++ array_interation.js
@@ -2,11 +2,11 @@
 A common operation on array in iteration. You will need this everyday in your career
  */
 
-let colors = ['red', 'green', 'blue']
+// let colors = ['red', 'green', 'blue']
 
-for (let i = 0; i < colors.length; i++) {
-    console.log(colors[i])
-  }
+// for (let i = 0; i < colors.length; i++) {
+//     console.log(colors[i])
+//   }
 /*
 In the for-loop we create variable i, and i is less than colors.length and increase i
  */
@@ -17,15 +17,27 @@ In the for-loop we create variable i, and i is less than colors.length and incre
 //     console.log(color);
 // }
 
-colors.forEach(color => console.log(color))
+// colors.forEach(color => console.log(color))
 
 
 // Unassigned values 
 
-let array = ['first', 'second', , 'fourth']
+// let array = ['first', 'second', , 'fourth']
 
-array.forEach(function(element) {
-    console.log(element)
-  })
+// array.forEach(function(element) {
+//     console.log(element)
+//   })
 
   //  Unassigned values are not iterated in a forEach loop.
+
+//   The arr.forEach method allows to run a function for every element of the array.
+
+// The syntax:
+
+// arr.forEach(function(item, index, array) {
+//   // ... do something with item
+// });
+
+["Bilbo", "Gandalf", "Nazgul"].forEach((item, index, array) => {
+    console.log(`${item} is at index ${index} in ${array}`);
+  });
