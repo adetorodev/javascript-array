df98eb172cbbe251ff8dada0bbf2e2d755e29a6e
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-02-28 06:51:59 -0500

    filter(fn)

--- 5_searching.js
+++ 5_searching.js
@@ -68,3 +68,15 @@
 //   // if true item is pushed to results and the iteration continues
 //   // returns empty array if nothing found
 // });
+
+let users = [
+        { id: 1, name: "John" },
+        { id: 2, name: "Pete" },
+        { id: 3, name: "Mary" }
+];
+
+// returns array of the first two users
+let someUsers = users.filter(item => item.id < 3);
+
+console.log(someUsers.length); // 2
+console.log(someUsers); 
