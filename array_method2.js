4a19b544bea950e17fad2bd361f0673f0760e33f
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-02-28 05:48:30 -0500

    slice intro

---
+++ array_method2.js
@@ -0,0 +1,12 @@
+/* *************************************
+        ARRAY SLICE
+*************************************** */
+
+// The method arr.slice is much simpler than similar-looking arr.splice.
+
+// The syntax is:
+
+// arr.slice([start], [end])
+
+// It returns a new array copying to it all items from index start to end (not including end). Both start 
+// and end can be negative, in that case position from array end is assumed.
