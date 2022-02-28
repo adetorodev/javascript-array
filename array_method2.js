eb94a872cbf1622f325ebac01ac7e85d2b8cf2cb
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-02-28 06:06:46 -0500

    slice example 2

--- array_method2.js
+++ array_method2.js
@@ -11,8 +11,16 @@
 // It returns a new array copying to it all items from index start to end (not including end). Both start 
 // and end can be negative, in that case position from array end is assumed.
 
-let arr = ["t", "e", "s", "t"];
+// let arr = ["t", "e", "s", "t"];
 
-console.log( arr.slice(1, 3) ); // e,s (copy from 1 to 3)
+// console.log( arr.slice(1, 3) ); // e,s (copy from 1 to 3)
 
-console.log( arr.slice(-2) ); // s,t (copy from -2 till the end)
+// console.log( arr.slice(-2) ); // s,t (copy from -2 till the end)
+
+let months = ["January", "February", "Monday", "Tuesday"];
+let days = months.splice(2, 2, "March", "April");
+
+console.log(days); // ["Monday", "Tuesday"]
+console.log(months); // ["January", "February", "March", "April"]
+
+// Remove the array from monts and replace it with "March", "April"
