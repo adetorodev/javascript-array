4e218a80241e59dc3ed4a2785b916827955fb6c0
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-02-28 06:03:57 -0500

    splice example 1

--- array_method2.js
+++ array_method2.js
@@ -10,3 +10,9 @@
 
 // It returns a new array copying to it all items from index start to end (not including end). Both start 
 // and end can be negative, in that case position from array end is assumed.
+
+let arr = ["t", "e", "s", "t"];
+
+console.log( arr.slice(1, 3) ); // e,s (copy from 1 to 3)
+
+console.log( arr.slice(-2) ); // s,t (copy from -2 till the end)
