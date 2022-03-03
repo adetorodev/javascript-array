c2c747e6d588de6767bdf310e795edfd4bf3cc43
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-03-03 18:52:06 -0500

    reduce with initial

--- 7_transform.js
+++ 7_transform.js
@@ -37,3 +37,15 @@
 // let value = arr.reduce(function(accumulator, item, index, array) {
 //     // ...
 //   }, [initial]);
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let result = arr.reduce((sum, current) => sum + current, 0);
+
+// console.log(result); 
+
+let arr = [];
+
+// Error: Reduce of empty array with no initial value
+// if the initial value existed, reduce would return it for the empty arr.
+arr.reduce((sum, current) => sum + current);
