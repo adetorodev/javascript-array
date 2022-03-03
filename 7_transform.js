9107a6d6a5e8557a1860e16b25b40dd953103ff6
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-03-03 18:46:00 -0500

    reduce/reduceRight

--- 7_transform.js
+++ 7_transform.js
@@ -7,15 +7,33 @@
 // let arr = [1, 2, 3, 4, 5];
 // arr.reverse();
 
-// alert( arr ); // 5,4,3,2,1
+// console.log( arr ); // 5,4,3,2,1
 
 // split and join
 // Here’s the situation from real life. We are writing a messaging app, and the person enters the comma-delimited list of receivers: John, Pete, Mary. But for us an array of names would be much more comfortable than a single string. How to get it?
 
 // The str.split(delim) method does exactly that. It splits the string into an array by the given delimiter delim.
-let names = 'Bilbo, Gandalf, Nazgul';
+// let names = 'Bilbo, Gandalf, Nazgul';
 
-let arr = names.split(', ');
+// let arr = names.split(', ');
 
-for (let name of arr) {
-  alert( `A message to ${name}.` ); // A message to Bilbo  (and other names)
+// for (let name of arr) {
+//   console.log( `A message to ${name}.` );
+// }
+
+// let str = "test";
+
+// alert( str.split('') ); // t,e,s,t
+
+// reduce/reduceRight
+// When we need to iterate over an array – we can use forEach, for or for..of.
+
+// When we need to iterate and return the data for each element – we can use map.
+
+// The methods arr.reduce and arr.reduceRight also belong to that breed, but are a little bit more intricate. They are used to calculate a single value based on the array.
+
+// The syntax is:
+
+// let value = arr.reduce(function(accumulator, item, index, array) {
+//     // ...
+//   }, [initial]);
