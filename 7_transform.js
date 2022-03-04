4559c18317be5e422be969639687037cbb42202d
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-03-03 19:10:05 -0500

    thanks

--- 7_transform.js
+++ 7_transform.js
@@ -44,8 +44,11 @@
 
 // console.log(result); 
 
-let arr = [];
+// let arr = [];
 
 // Error: Reduce of empty array with no initial value
 // if the initial value existed, reduce would return it for the empty arr.
-arr.reduce((sum, current) => sum + current);
+// console.log(arr.reduce((sum, current) => sum + current));
+// TypeError: Reduce of empty array with no initial value
+
+// Thanks
